150c9ccd9b05faeca7e1902f3446ed0d5e869629
elijahb22 <elibusick11@gmail.com>
2022-04-24 23:44:32 -0400

    created nav bar

---
+++ src/components/Nav/nav.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const nav = () => {
+
+    
+
+  return (
+    <div className=''>
+        <div className=''>
+            <div className=''>
+                <h1> 
+                    <a href="/">Elijah Busick </a>
+                </h1>
+            </div>
+            <div className=''>
+                <ul>
+                    <li>
+                        <a href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact Me</a>
+                    </li>
+                    <li>
+                        <a href="#about">Resume</a>
+                    </li>
+                </ul>
+            </div>
+           
+        </div>
+    </div>
+  )
+}
+
+export default nav
