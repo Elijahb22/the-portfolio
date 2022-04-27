d31757907b26b69febb9406c02e814cbd02c7760
elijahb22 <elibusick11@gmail.com>
2022-04-27 10:14:02 -0400

    created about sections

---
+++ src/components/About/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const intro = () => {
+  return (
+    <section className=''> 
+        <div className=''> 
+            <div className=''>
+                <h2 className=''> Hello, my name is</h2>
+                <h1 className=''> Elijah Busick</h1>
+                <p className=''>
+                I am a full-stack Web developer who recently earned a Full Stack Development Certificate from the University of North Carolina at Charlotte .I have furthered my skills in the front-end applications such as HTML, Bootstrap, jQuery, and React.js. Regarding back-end applications, I am efficient in Node.js, Express.js, MySQL, and MongoDB. My passion is to develop projects that appeal to all users and prioritize user friendliness and easy navigation to maximize value. Utilizing these skills, I have worked with a team to develop a front-end application, an interactive full-stack program, and an Interactive MERN app. These projects required me to be detail oriented, task-organized and collaborate efficiently with others. I look forward to leveraging my background in digital marketing to create a high-quality experience for customers using web applications.
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default intro
