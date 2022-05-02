7cae44c5135d89f1ab9548a93a3073a6dc87d82a
elijahb22 <elibusick11@gmail.com>
2022-05-02 16:47:32 -0400

    added other icons and styled them

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -4,22 +4,68 @@ import { Col, Container, Row } from "react-bootstrap";
 import {
     DiJavascript1,
     DiReact,
+    DiNodejs,
+    DiMongodb,
+    DiCss3
 } from "react-icons/di";
+import { AiFillHtml5 } from "react-icons/ai"
+import { 
+    SiMysql,
+    SiExpress,
+    SiHeroku,
+    SiVisualstudiocode,
+    SiInsomnia
+} from "react-icons/si"
+import { FiFigma } from "react-icons/fi"
 import './skills.css'
 const skill = () => {
-
     return (
     <div className="page">
     <Container>
-    <h1> Professional Skillset </h1>
+    <h2 className="h1-s"> Professional Skillset </h2>
     <Row>
         <Col className="tech-icons">
             <DiJavascript1 />
         </Col>
-        <Col  className="tech-icons">
-            <DiReact />
+        <Col className="tech-icons">
+            <DiReact alt />
+        </Col>
+        <Col className="tech-icons">
+            <DiNodejs />
+        </Col>
+        <Col className="tech-icons">
+            <DiMongodb />
         </Col>
     </ Row>
+    <Row>
+        <Col className="tech-icons">
+                <AiFillHtml5 />
+        </Col>
+        <Col className="tech-icons">
+            <DiCss3 />
+        </Col>
+        <Col className="tech-icons">
+            <SiMysql />
+        </Col>
+        <Col className="tech-icons">
+            <SiExpress />
+        </Col>
+    </Row>
+    <h2 className="h1-s">Tools Used</h2>
+    <Row>
+        <Col className="tech-icons">
+            <SiHeroku />
+        </Col>
+        <Col className="tech-icons">
+            <FiFigma />
+        </Col>
+        <Col className="tech-icons">
+            <SiVisualstudiocode />
+        </Col>
+        <Col className="tech-icons">
+            <SiInsomnia />
+        </Col>
+    </Row>
     </Container>
     </div>
     )
