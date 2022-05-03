9ad879535bd3c9075fb957835cccadb1628c8c08
elijahb22 <elibusick11@gmail.com>
2022-05-03 10:41:31 -0400

    added portfolio file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './components/Nav/nav'
 import About from './components/About/about'
 import Skill from './components/Skills/skills'
+import Portfolio from './components/Portfolio/portfolio';
 const App = () => {
   return (
   <div >
     <Nav/>
     <About/>
     <Skill />
+    <Portfolio />
   </div>
 )
 };
---
+++ src/components/Portfolio/portfolio.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+
+const portfolio = () => {
+  return (
+    <section className=''> 
+        <div className=''>
+            <div className=''></div>
+            <div className=''></div>
+            <div className='p'></div>
+        </div>
+        <a href={link} target="_blank" 
+        rel="">
+            <img src={img} alt=" " className=''/>
+        </a>
+    </section>
+  )
+}
+
+export default portfolio
