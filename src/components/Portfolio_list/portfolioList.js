d88458420a0b5f470b58e9accb94d4f833b71f7e
elijahb22 <elibusick11@gmail.com>
2022-05-03 11:41:22 -0400

    add portfolio list page

---
+++ src/components/Portfolio_list/portfolioList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Portfolio from "../Portfolio/Portfolio.jsx"
+import { portfolio } from '../../data'
+
+const PortfolioList = () => {
+  return (
+    <section id='portfolio' className=''>
+      <div className=''>
+        <h1 className=''> Portfolio</h1>
+        {/* <p className='pl-desc'> Write description about portfolio</p> */}
+      </div>
+      <div className=''>
+        
+      </div>
+    </section>
+  )
+}
+
+export default PortfolioList
