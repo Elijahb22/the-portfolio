4a4d880d639f207f5e5f0b614506c64596be4019
elijahb22 <elibusick11@gmail.com>
2022-05-26 10:06:40 -0400

    styled footer icons

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,21 +2,21 @@ import React from 'react'
 import { Col, Container, Row } from "react-bootstrap";
 import { AiOutlineGithub, AiOutlineLinkedin} from 'react-icons/ai';
 import { BsReverseLayoutTextSidebarReverse } from 'react-icons/bs'
-
+import './footer.css'
 const footer = () => {
     return (
-        <Container>
-            <Row>
+        <Container className='con-f'>
+            <Row className='r-f'>
                 <Col>
-                    <p>Desinged and Developed by Elijah Busick</p>
+                    <p className='c-f'>Designed and Developed by Elijah Busick</p>
+                </Col>
+                <Col xs={4}>
+                    <p className='c-f'>Copyright @ 2022 EB</p>
                 </Col>
                 <Col>
-                    <p>Copyright 2022 EB</p>
-                </Col>
-                <Col>
-                    <AiOutlineGithub />
-                    <AiOutlineLinkedin />
-                    <BsReverseLayoutTextSidebarReverse />
+                    <a href="https://github.com/Elijahb22" alt="Github"><AiOutlineGithub className='icons-f'/> </a>
+                    <a href="https://www.linkedin.com/in/elijah-busick-644487187/" alt="Linkedin"><AiOutlineLinkedin className='icons-f'/></a>
+                    <a href='https://docs.google.com/document/d/1DZuejBSMMyzH2-ELE29exDj24cpNRsD-xFGISJRgM90/edit?usp=sharing' alt="Learning Plan"><BsReverseLayoutTextSidebarReverse className='icons-f' /></a>
                 </Col>
             </Row>
         </Container>
