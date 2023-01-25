c00978b6406366c4da73f2b7946b56bc70a211a0
elijahb22 <elibusick11@gmail.com>
2023-01-25 09:06:29 -0500

    fix nav

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -3,33 +3,30 @@ import './nav.css'
 const nav = () => {
 
   return (
-    <div className=''>
-            <nav class="navbar bg-body-tertiary">
-                <div class="container-fluid">
-                    <span class="navbar-brand mb-0 h1">Elijah</span>
-                </div>
-            
-            <div className='nav-w'>
-                <ul class="nav justify-content-end">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Elijah</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#about">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#skill">Skills</a>
+                        <a class="nav-link" href="#skills">Skills</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#portfolio">Projects</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="contact">Contact Me</a>
+                        <a class="nav-link" href="#contact">Contact Me</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#resume">Resume</a>
                     </li>
                 </ul>
             </div>
-            </nav>
-    </div>
+        </div>
+    </nav>
   )
 }
 
