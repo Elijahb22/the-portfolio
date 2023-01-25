8022fd491707ae3171e7bdc3775a7e8e22c1834d
elijahb22 <elibusick11@gmail.com>
2023-01-25 08:46:36 -0500

    fixing structure of page

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -4,12 +4,12 @@ const nav = () => {
 
   return (
     <div className='nav'>
-            <nav class="navbar bg-body-tertiary">
-                <div class="container-fluid">
-                    <span class="navbar-brand mb-0 h1">Elijah</span>
-                </div>
-            </nav>
             <div className='nav-w'>
+                <nav class="navbar bg-body-tertiary">
+                    <div class="container-fluid">
+                        <span class="navbar-brand mb-0 h1">Navbar</span>
+                    </div>
+                </nav>
                 <ul class="nav justify-content-end">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#about">About</a>
