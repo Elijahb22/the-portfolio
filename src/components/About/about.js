cdad2c89d253bb28edd7e574940bbce6adb497d2
elijahb22 <elibusick11@gmail.com>
2023-01-24 20:42:20 -0500

    center about text

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -11,7 +11,7 @@ const about = () => {
               </div>
                 <h2 className='about-i'> Hello, my name is</h2>
                 <h1 className='about-name'> Elijah Busick</h1>
-                <p className=''>
+                <p className='center-text'>
                 I recently graduated from the jump program where I study java to pursue my passion in the tech industry. I obtained my bachelors degree in marketing and sales where I developed my collaboration/team work skills. I also acquired a certification as a Full stack web developer where I advanced my skills in Javascript and MySQL. In tech, I know Java, MySQL, and Javascript. I strive to be a senior developer at a fortuned 500 company one day by using my knowledge in java and adaptability skills.
                 </p>
             </div>
