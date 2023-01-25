239035d177a2ba84c6195c0f7092054bc6cfe4f2
elijahb22 <elibusick11@gmail.com>
2023-01-25 09:58:10 -0500

    size Elements

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -5,12 +5,12 @@ const nav = () => {
   return (
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Elijah</a>
+            <a class="navbar-brand f2-1" href="#">Elijah</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="nav-w" id="navbarNav">
-                <ul class="navbar-nav fs-2">
+                <ul class="navbar-nav fs-4">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#about">About</a>
                     </li>
