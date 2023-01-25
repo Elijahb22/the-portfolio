d6c50e5a1d332df20baead7fb3a25a309bc7f307
elijahb22 <elibusick11@gmail.com>
2023-01-25 10:14:51 -0500

    remove button

--- src/components/Nav/nav.js
+++ src/components/Nav/nav.js
@@ -6,9 +6,7 @@ const nav = () => {
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand fs-1" href="#">Elijah</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-            </button>
             <div class="nav-w" id="navbarNav">
                 <ul class="navbar-nav fs-4">
                     <li class="nav-item">
